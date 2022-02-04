42cd155407360cff80886c343f6c77eb3bb47d08
Sal Savage <salsavage@google.com>
2022-02-04 09:41:57 -0800

    surya: Enable bluetooth profiles with product.prop
    Bug: 217448211
    Test: make -j
    Change-Id: Idd7e52b7822915ed61299f1c0b29bf87ca0339fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,9 @@ DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += surya
 ODM_MANIFEST_SURYA_FILES := $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
 
+# Props
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya
 TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
