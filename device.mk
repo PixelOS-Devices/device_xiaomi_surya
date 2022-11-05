91709cfa14bff68d0c6cb881b6c35b38c4381148
Lostark13 <tushar9915@gmail.com>
2022-11-05 19:22:08 +0530

    surya: Update media configs from V13.0.4.0.SJGIDXM
    Change-Id: I583482952d114bb30bfe7bcd504b21c0592a4d0d

--- device.mk
+++ device.mk
@@ -175,7 +175,9 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_sdmmagpie_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_vendor_sdmmagpie_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
 # Service Tracker
 PRODUCT_PACKAGES += \
